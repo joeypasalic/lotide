849dd895aca063fc895c3fb7e91b6e372cd634f7
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-06 16:18:39 -0500

    Create assertEqualsArray.js
    implemented function assertEqualsArray which console.logs a message regarding the comparasion of 2 arrays

---
+++ assertEqualsArray.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+
+  for (let i = 0; i < arr1.length; i++) {
+  
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  
+  if (arr1.length !== arr2.length) {
+    console.log("this array is false");
+  }
+
+
+  for (let i = 0; i < arr1.length; i++) {
+  
+    if (arr1[i] !== arr2[i]) {
+      console.log("this array is false");
+    }
+  }
+
+  console.log("this array is true");
+};
+
+// Test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true);
