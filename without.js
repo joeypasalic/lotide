8af0aa051e212437b12ce90060331e9235262002
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-06 17:03:01 -0500

    implemented a loop instead of .without method stated from lodash, as well as a test code given by the feedback AI

--- without.js
+++ without.js
@@ -26,7 +26,16 @@ const assertArraysEqual = function(arr1, arr2) {
 
 const without = function(source, itemsToRemove) {
 
-  return source.filter(item => !itemsToRemove.includes(item));
+  const filteredArr = [];
+
+  for (let item of source) {
+
+    if (!itemsToRemove.includes(item)) {
+      filteredArr.push(item);
+    }
+  }
+
+  return filteredArr;
 
 };
 
@@ -39,4 +48,5 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => should PASS
 
