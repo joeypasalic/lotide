70fd9bde9a65730358b2ee07d75c6623d008f09b
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-07 16:15:06 -0500

    changed for...in loop to for...of, as well as edited the placement of else if statment on line 42

--- eqObjects.js
+++ eqObjects.js
@@ -33,18 +33,18 @@ const eqObjects = function(obj1, obj2) {
     return false;
   }
 
-  for (const key in keys1) {
+  for (const key of keys1) {
 
     if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
       if (!eqArrays(obj1[key], obj2[key])) {
         return false;
-
-      } else if (obj1[key] !== obj2[key]) {
-
-        return false;
       }
+    } else if (obj1[key] !== obj2[key]) {
+
+      return false;
     }
   }
+
   return true;
 };
 
