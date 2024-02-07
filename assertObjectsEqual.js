8ab75e1d33f9607b00d4077a699adab3542d4fec
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-07 16:56:48 -0500

    corrected the implementation of assertObjectsEqual and eqObjects, fixed use of $inspect and improved test functions to pass objects to assertObjectsEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -9,9 +9,15 @@ const eqObjects = function(obj1, obj2) {
     return false;
   }
 
+
+
   for (const key of keys1) {
 
-    if (obj1[key] !== obj2[key]) {
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      if (obj1[key] !== obj2[key]) {
+        return false;
+      }
+    } else if (obj1[key] !== obj2[key]) {
 
       return false;
     }
@@ -24,20 +30,20 @@ const eqObjects = function(obj1, obj2) {
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
 
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  if (!eqObjects(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion failed: ${inspect(actual)} !== ${inspect(expected)}`);
 
   } else {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion passed: ${inspect(actual)} === ${inspect(expected)}`);
   }
 
 };
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
-assertObjectsEqual(eqObjects(shirtObject, anotherShirtObject), true); // => true
+assertObjectsEqual(shirtObject, anotherShirtObject); // => true
 
 const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
-assertObjectsEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // => false
-assertObjectsEqual(eqObjects(shirtObject, longSleeveShirtObject), true); // => false should fail
+assertObjectsEqual(shirtObject, longSleeveShirtObject); // => false
+assertObjectsEqual(shirtObject, longSleeveShirtObject); // => false should fail
 
