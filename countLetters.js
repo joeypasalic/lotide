31dc93fa063ba576977c953960df606ceb3e9045
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-07 13:45:22 -0500

    implemented countLetters function which returns an array of the amount of letters in the given parameter

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const results = [];
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter]++;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const thing = countLetters('LHL');
+assertEqual(thing['L'], 2);
+assertEqual(thing['H'], 1);
