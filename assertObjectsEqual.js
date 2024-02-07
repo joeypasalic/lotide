da3e3ac15f614886b6c8c5cb768241c6702fe8db
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-07 17:05:51 -0500

    added eqArrays as a helper function tot help with comparing contents of arrays acurately

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,18 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
 const eqObjects = function(obj1, obj2) {
 
   const keys1 = Object.keys(obj1);
@@ -14,7 +29,7 @@ const eqObjects = function(obj1, obj2) {
   for (const key of keys1) {
 
     if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
-      if (obj1[key] !== obj2[key]) {
+      if (!eqArrays(obj1[key], obj2[key])) {
         return false;
       }
     } else if (obj1[key] !== obj2[key]) {
