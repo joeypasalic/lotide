86994c37d74016b3143c921ba048d7a866de0bc2
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-12 23:22:23 -0500

    created assertArraysEqualTest.js which handles the testing of assertArraysEqual

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+// Test cases
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
