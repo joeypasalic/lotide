84a7d27ad79e6483d44a2fa8982b436842a55aec
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-12 23:23:16 -0500

    dried up wet code by requiring functions and separating testing into another file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,27 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-
-  for (let i = 0; i < arr1.length; i++) {
-  
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
 
@@ -32,8 +9,5 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-// Test cases
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
+
