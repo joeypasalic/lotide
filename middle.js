a8a7d3d52726a35aba3567a1c1ef42748c2e2cfb
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-12 23:29:33 -0500

    dried up middle.js code to require assertArraysEqual as well as export its module to use as testing in middleTest.js

--- middle.js
+++ middle.js
@@ -1,28 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-
-  for (let i = 0; i < arr1.length; i++) {
-  
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(arr) {
 
@@ -45,11 +21,4 @@ const middle = function(arr) {
 
 };
 
-
-
-// Test cases
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+module.exports = middle;
